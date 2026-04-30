4ff25ba3369d8506cc928071598ca2eefda7dc6c
Filip Vrba <filipvrbaxi@gmail.com>
2026-04-30 15:07:57 +0200

    lol

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,30 @@
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   root: 'vite_dev',
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    emptyOutDir: true,
   },
   plugins: [
     {
       name: 'watch-mustela',
       configureServer(server) {
-        server.watcher.add('./vite_dev/*.html');
+        // server.watcher.add('./vite_dev/*.html');
+        server.watcher.add('./vite_dev/**/*.{html,xml,json}');
         server.watcher.on('change', () => {
           server.ws.send({ type: 'full-reload', path: '*' });
         });
       }
-    }
+    },
+    viteStaticCopy({
+      targets: [
+        {
+          src: ['sitemap.xml', 'feed.xml', 'articles.json'],
+          dest: './'
+        }
+      ]
+    })
   ]
 });
